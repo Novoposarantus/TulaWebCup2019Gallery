d8d12cd4597111a21e1a02cca948824a52805e96
Novoposarantus <novoposarantu1@gmail.com>
2019-04-15 23:12:26 +0300

    Add sorting

--- gallery-web/src/components/layout/Gallery/GalleryHeader.vue
+++ gallery-web/src/components/layout/Gallery/GalleryHeader.vue
@@ -2,40 +2,106 @@
     <div>
         <div class="galery-header">
             <v-btn
+                class="choose-image"
                 v-if="isAuthenticated"
                 @click="loadImage">
                 <v-icon>fas fa-file-image</v-icon>
                 <span class="hidden-xs-only button-text">Загрузить изображения</span>
             </v-btn>
+            <v-select
+                @change="changeSortBy"
+                class="filter-select"
+                :items="filterItems"
+                label="Фильтрация"
+            ></v-select>
+            <v-switch
+                class="choose-image"
+                v-model="reverseSort"
+                :label="filterReverseName"
+            ></v-switch>
         </div>
     </div>
 </template>
 
 <script>
-import {mapGetters} from 'vuex';
-import {routeNames, authGlobalGetters} from '@/support';
+import {mapGetters, mapActions} from 'vuex';
+import {
+    routeNames, 
+    authGlobalGetters, 
+    galleryGlobalGetters, 
+    galleryGlobalActions, 
+    sort
+} from '@/support';
 
 export default {
-    methods:{
-        loadImage(){
-            this.$router.push({name: routeNames.LoadImages});
+    data(){
+        return {
+            filterItems:[
+                {
+                    text: 'По дате',
+                    value: sort.none
+                },
+                {
+                    text: 'По имени',
+                    value: sort.name
+                },
+                {
+                    text: 'По рейтингу',
+                    value: sort.rating
+                }
+            ]
         }
     },
     computed:{
         ...mapGetters({
-            isAuthenticated: authGlobalGetters.isAuthenticated
-        })
+            isAuthenticated: authGlobalGetters.isAuthenticated,
+            filter: galleryGlobalGetters.filter
+        }),
+        filterReverseName(){
+            if(this.filter.reverseSort){
+                return "По убыванию";
+            }
+            return "По возрастанию"
+        },
+        reverseSort:{
+            get(){
+                return this.filter.reverseSort;
+            },
+            set(value){
+                this.setReverseSort(value);
+            }
+        }
+    },
+    methods:{
+        ...mapActions({
+            setSort: galleryGlobalActions.setSortBy,
+            setReverseSort: galleryGlobalActions.setReverseSort
+        }),
+        loadImage(){
+            this.$router.push({name: routeNames.LoadImages});
+        },
+        changeSortBy(sortId){
+            this.setSort(sortId);
+        }
     }
 }
 </script>
 
 <style scoped>
 .galery-header{
-    display: flex;
     box-sizing: border-box;
     padding: 20px; 
 }
 .button-text{
     margin-left: 10px;
+}
+.choose-image{
+    display: inline-block;
+    margin: 0 20px;
+}
+.filter-select{
+    display: inline-block;
+    max-width: 150px;
+    margin: 0 20px;
 }
 </style>
